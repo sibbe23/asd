5de770560d8e590b8ac6c5d151416f7c159f4daa
sibbe23 <sibbe23@gmail.com>
2022-12-29 18:47:03 +0530

    Colors

---
+++ Qs.js
@@ -0,0 +1,8 @@
+var sec = document.getElementsByTagName('li')
+sec[1].style.backgroundColor="lightblue";
+sec[2].style.display="none"
+var x = document.querySelectorAll('li')
+x[1].style.color="green"
+for(var i=0;i<x.length;i=i+2)
+x[i].style.backgroundColor = "lightgreen"
+
