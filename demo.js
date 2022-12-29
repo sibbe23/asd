ac62dff31763141d9adfaba458c957014e246d14
sibbe23 <sibbe23@gmail.com>
2022-12-29 17:51:49 +0530

    Done

---
+++ demo.js
@@ -0,0 +1,5 @@
+var header =document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var bolds = document.getElementById('help');
+bolds.style.color ="green";
+bolds.style.fontWeight = "bold";
