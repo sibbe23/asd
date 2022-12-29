fb0e465f200db49a6d6785c9f79d5c1c7badeb37
sibbe23 <sibbe23@gmail.com>
2022-12-29 18:06:22 +0530

    Bold

---
+++ dom.js
@@ -0,0 +1,5 @@
+var header = document.getElementsByClassName('list-group-item');
+header[2].style.backgroundColor = "lightgreen";
+for(var i =0;i<header.length;i++)
+header[i].style.fontWeight = "bold"
+
